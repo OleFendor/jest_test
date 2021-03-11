6ad6c3728a4c220cdea7f760b70e60469c4d695f
OleFendor <o.fedorenk2010@gmail.com>
2021-03-11 19:05:24 +0200

    create tests

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -24,4 +24,36 @@ describe('create tests', () => {
   test('undefined object', () => {
     expect(controller.create()).toThrow()
   })
+  test('empty user', () => {
+    const user = new User()
+    expect(controller.create(user)).toThrow()
+  })
+  test('without all fields', () => {
+    const user = new User({
+      name: 'a',
+    })
+    expect(controller.create(user)).toThrow()
+  })
+  test('wronge role', () => {
+    const user = new User({
+      name: 'a',
+      surname: 'b',
+      email: 'c',
+      password: 'd',
+      role: 'e',
+    })
+    expect(controller.create(user)).toThrow()
+  })
+
+  test('success create', async () => {
+    const user = new User({
+      name: 'a',
+      surname: 'b',
+      email: 'c',
+      password: 'd',
+      role: 'User',
+    })
+    await controller.create(user)
+    expect(controller.getAll()).toEqual(user)
+  })
 })
