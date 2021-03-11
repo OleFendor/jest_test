62c1fea6d09e4b9850ab874b60c61197a525ae6c
OleFendor <o.fedorenk2010@gmail.com>
2021-03-11 16:30:15 +0200

    Controllers

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,22 +1,66 @@
-const mongoose = require('mongoose')
+const { findById } = require('../models/todo')
+const Todo = require('../models/todo')
 
-const Schema = mongoose.Schema
+exports.create = async (todo) => {
+  try {
+    await new Todo({
+      name: todo.name,
+      description: todo.description,
+      status: todo.status,
+      date: todo.date,
+      user: todo.user,
+    }).save()
+    return 'success'
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-const userSchema = new Schema({
-  name: String,
-  surname: String,
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['Admin', 'User'],
-    default: 'User',
-  },
-})
-module.exports = mongoose.model('User', userSchema)
+exports.getAll = async () => {
+  try {
+    const todos = await Todo.find({})
+    return todos
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.getById = async (id) => {
+  try {
+    const todo = await Todo.findById(id)
+    return todo
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.getByUser = async (user) => {
+  try {
+    const todo = await Todo.findOne({ user: user })
+    return todo
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.update = async (newdata) => {
+  try {
+    const todo = await findById(newdata._id)
+    await todo.update({
+      name: newdata.name ?? todo.name,
+      description: newdata.description ?? todo.description,
+      status: newdata.status ?? todo.status,
+      date: newdata.date ?? todo.date,
+      user: newdata.user ?? todo.user,
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+exports.delete = async (id) => {
+  try {
+    await Todo.findByIdAndDelete(id)
+  } catch (e) {
+    console.error(e)
+  }
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,65 @@
-const mongoose = require('mongoose')
+const User = require('../models/user')
 
-const Schema = mongoose.Schema
+exports.create = async (user) => {
+  try {
+    await new User({
+      name: user.name,
+      surname: user.surname,
+      email: user.email,
+      password: user.password,
+      role: user.role,
+    }).save()
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-const userSchema = new Schema({
-  name: String,
-  surname: String,
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['Admin', 'User'],
-    default: 'User',
-  },
-})
-module.exports = mongoose.model('User', userSchema)
+exports.getById = async (id) => {
+  try {
+    const user = await User.findById(id)
+    return user
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.getByEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email: email })
+    return user
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.getAll = async () => {
+  try {
+    const users = await User.find({})
+    return users
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.delete = async (id) => {
+  try {
+    await User.findByIdAndDelete(id)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.update = async (newdata) => {
+  try {
+    const user = await User.findById(newdata._id)
+    await user.update({
+      name: newdata.name ?? user.name,
+      surname: newdata.surname ?? user.surname,
+      email: newdata.email ?? user.email,
+      password: newdata.password ?? user.password,
+      role: newdata.role ?? user.role,
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
